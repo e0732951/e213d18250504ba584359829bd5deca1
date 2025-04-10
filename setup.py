5f673cbf354d146efd9ad04442faba74ccfda382
e0732951 <105834806+e0732951@users.noreply.github.com>
2025-04-10 23:55:11 +0800

    Setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='e213d18250504ba584359829bd5deca1',  
+    version='1.0.0',
+    description='An example package',
+    url='https://github.com/e0732951/e213d18250504ba584359829bd5deca1/',
+    author='javerngoh',
+    author_email='e0732951.u.nus.edu',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=2.0.2',
+    ],
+)
