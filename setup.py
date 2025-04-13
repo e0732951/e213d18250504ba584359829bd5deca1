fee268639f9aa72f9686da78bf8f62077af3edcf
e0732951 <105834806+e0732951@users.noreply.github.com>
2025-04-13 14:11:14 +0800

    packages1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='e213d18250504ba584359829bd5deca1',  
-    version='1.0.1',
+    version='1.0.2',
     description='An example package',
     url='https://github.com/e0732951/e213d18250504ba584359829bd5deca1/',
     author='javerngoh',
