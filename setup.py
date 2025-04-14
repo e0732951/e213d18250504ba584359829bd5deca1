437e2567d8e41a60ed92117c274b485409de89fc
e0732951 <105834806+e0732951@users.noreply.github.com>
2025-04-15 00:19:33 +0800

    hash

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='e213d18250504ba584359829bd5deca1',  
-    version='1.0.5',
+    version='1.0.6',
     description='An example package',
     url='https://github.com/e0732951/e213d18250504ba584359829bd5deca1/',
     author='javerngoh',
